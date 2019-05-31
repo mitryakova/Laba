b2dbac80948788ee532e0f5519ac681c6aae3303
Ira <mitryascha1507@yandex.ru>
2019-05-31 13:04:04 +0700

    IStorage added

---
+++ Laba/Models/IStorage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Laba.Models
+{
+   
+        public interface IStorage<T> where T : class
+        {
+            List<T> All { get; }
+            T this[Guid id] { get; set; }
+            void Add(T value);
+            void RemoveAt(Guid id);
+            bool Has(Guid id);
+        }
+    
+}
